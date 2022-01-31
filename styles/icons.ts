54f48ed17cecd42e380bbea680220125e4c37187
Brighten Tompkins <brightenqtompkins@gmail.com>
2022-01-31 09:21:35 -0800

    feat(footer.tsx): modified footer to align differently based on screen size

--- styles/icons.ts
+++ styles/icons.ts
@@ -3,7 +3,6 @@ import { IconButton } from '@mui/material';
 
 export const StyledIconButton = styled(IconButton)`
   color: 'white';
-  padding-top: 0.5em;
   padding-left: 0.125em;
   padding-right: 0.125em;
 `;
