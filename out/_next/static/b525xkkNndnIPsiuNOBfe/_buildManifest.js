5582c89deaa46578373b99db1f18cd88ad628f26
Brighten Tompkins <brightenqtompkins@gmail.com>
2022-04-07 13:42:35 -0700

    feat(information.tsx): corrected location information

--- out/_next/static/b525xkkNndnIPsiuNOBfe/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-40e455150bc762262382.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-927dcf93581c925cc38b.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
