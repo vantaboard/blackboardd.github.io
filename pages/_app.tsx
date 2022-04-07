b424fc0971e4adde5ea643ea2135b4638565fa1c
Brighten Tompkins <brightenqtompkins@gmail.com>
2022-04-07 13:56:44 -0700

    \Deploy to page\

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import type { AppProps } from 'next/app';
-import './index.css';
-
-function blackboarddGitHubIO({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
-}
-
-export default blackboarddGitHubIO;
