eb159631d9b09265bcaee0c573cda53e0101e68c
brighten <brightenqtompkins@gmail.com>
2023-05-10 03:51:28 -0700

    :bug: (styling) it should work now

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import type { AppProps } from 'next/app';
 import './index.css';
+import StyledComponentsRegistry from '@/lib/registry';
+
+function Layout({ children }: { children: React.ReactNode }) {
+    return <StyledComponentsRegistry>{children}</StyledComponentsRegistry>;
+}
 
 function vantaboardGitHubIO({ Component, pageProps }: AppProps): JSX.Element {
-    return <Component {...pageProps} />;
+    return (
+        <Layout>
+            <Component {...pageProps} />;
+        </Layout>
+    );
 }
 
 export default vantaboardGitHubIO;
